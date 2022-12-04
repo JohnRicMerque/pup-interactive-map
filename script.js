9dbb7ff7d48501771e9d56103f352fa4cf29b2b8
John Ric Merque <johnricmer@gmail.com>
2022-12-04 12:15:53 +0800

    map functional, added preface

---
+++ script.js
@@ -0,0 +1,73 @@
+function showSceneryBox(imgSource, heading, description) {
+    let display = document.getElementById("display");
+    document.addEventListener('mousemove', displayFollowCamera);
+    display.innerHTML = "<div class='wrapper'><img class='imgScene' src='" + imgSource + "'> "+ "<p class='building-name'>" + heading + "</p>" + description + "</p></div>";
+}
+
+function displayFollowCamera() {
+    let display = document.getElementById("display");
+
+    if (event.clientY > 350) {
+        display.style.top = event.clientY - 150 + "px";
+    } else {
+        display.style.top = event.clientY + 50 + "px";
+    }
+ 
+    console.log(event.clientY)
+
+    if (event.clientX > 900) {
+        display.style.left = event.clientX - 300 + "px";
+    } else {
+        display.style.left = event.clientX + 50 + "px";
+    }
+}
+
+function removeSceneryBox() {
+    let display = document.getElementById('display')
+    document.removeEventListener('mousemove', displayFollowCamera);
+    display.removeChild(display.childNodes[0]);
+}
+
+function showMap() {
+    const map = document.getElementById('map-container');
+    const preface = document.getElementById('preface-container')
+    const showbtn = document.getElementById('show-map');
+    if (map.style.display == "flex"){
+        map.style.display = "none"
+    } else {
+        map.style.display = "flex"
+    }
+
+    if (preface.style.display == "none"){
+        preface.style.display = "grid"
+    } else {
+        preface.style.display = "none"
+    }
+}
+
+$(function() {
+    $('.map').maphilight();
+});
+$.fn.maphilight.defaults = {
+fill: true,
+fillColor: '000000',
+fillOpacity: 0.4,
+stroke: true,
+strokeColor: '630000',
+strokeOpacity: 1,
+strokeWidth: 4,
+fade: true,
+alwaysOn: false,
+neverOn: false,
+groupBy: false,
+wrapClass: true,
+shadow: false,
+shadowX: 0,
+shadowY: 0,
+shadowRadius: 6,
+shadowColor: '000000',
+shadowOpacity: 0.8,
+shadowPosition: 'outside',
+shadowFrom: false
+}
+
